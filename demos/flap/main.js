be8e7f314a0bea76b82434393578c0bf0b48d154
Diogo Schneider <dgsprb@gmail.com>
2015-04-02 20:43:07 -0300

    Add demos

---
+++ demos/flap/main.js
@@ -0,0 +1,233 @@
+(function () {
+
+	"use strict";
+
+	// imports
+	var Animation = com.dgsprb.quick.Animation;
+	var BaseTransition = com.dgsprb.quick.BaseTransition;
+	var CommandEnum = com.dgsprb.quick.CommandEnum;
+	var Quick = com.dgsprb.quick.Quick;
+	var Frame = com.dgsprb.quick.Frame;
+	var GameObject = com.dgsprb.quick.GameObject;
+    var ImageFactory = com.dgsprb.quick.ImageFactory;
+	var Rect = com.dgsprb.quick.Rect;
+    var Scene = com.dgsprb.quick.Scene;
+
+	// constants
+	var CLOUD_IMAGE = document.getElementById("cloudSprite");
+	var CLOUD_IMAGE_MIRROR = ImageFactory.mirror(CLOUD_IMAGE);
+
+	// functions
+	function main() {
+		Quick.setName("Flap Demo");
+		Quick.setNumberOfLayers(3);
+		Quick.init(document.getElementById("canvas"), GameScene);
+	}
+
+	// classes
+	var Background = (function () {
+
+		function Background() {
+			GameObject.call(this);
+			this.setColor("Blue");
+			this.setSize(Quick.getCanvasWidth(), Quick.getCanvasHeight());
+		}; Background.prototype = Object.create(GameObject.prototype);
+
+		return Background;
+
+	})();
+
+	var Cloud = (function () {
+
+		function Cloud() {
+			GameObject.call(this);
+			this.setImage(CLOUD_IMAGE);
+			this.setBoundary(new Rect(0, 0, Quick.getCanvasWidth(), Quick.getCanvasHeight()));
+		}; Cloud.prototype = Object.create(GameObject.prototype);
+
+		// override
+		Cloud.prototype.offBoundary = function () {
+			this.setLeft(Quick.getCanvasWidth() - 1);
+		};
+
+		return Cloud;
+
+	})();
+
+	var Column = (function () {
+
+		function Column() {
+			GameObject.call(this);
+			this.addTag("column");
+			this.setColor("DarkGray");
+			this.setHeight(Quick.getCanvasHeight());
+			this.setSolid();
+		}; Column.prototype = Object.create(GameObject.prototype);
+
+		return Column;
+
+	})();
+
+	var Fog = (function () {
+
+		function Fog() {
+			GameObject.call(this);
+			this.setLayerIndex(2);
+		}; Fog.prototype = Object.create(GameObject.prototype);
+
+		// override
+		Fog.prototype.update = function () {
+			if (this.getLeft() > -1 * this.getSpeedX() || this.getRight() < Quick.getCanvasWidth() - this.getSpeedX()) {
+				var speed = this.getSpeedX();
+				this.moveX(Quick.getCanvasWidth() * -1 * speed / Math.abs(speed));
+			}
+		};
+
+		return Fog;
+
+	})();
+
+	var GameScene = (function () {
+
+		function GameScene() {
+			Scene.call(this);
+			this.add(new Background());
+			var cloud1 = new Cloud();
+			cloud1.setSpeedX(-0.5);
+			this.add(cloud1);
+			var cloud2 = new Cloud();
+			cloud2.setSpeedX(-1);
+			cloud2.setRight(Quick.getCanvasWidth());
+			cloud2.setImage(CLOUD_IMAGE_MIRROR);
+			this.add(cloud2);
+			var fog1 = new Fog();
+			fog1.setImageId("fogSprite0");
+			fog1.setRight(Quick.getCanvasWidth());
+			fog1.setSpeedX(1);
+			this.add(fog1);
+			var fog2 = new Fog();
+			fog2.setImageId("fogSprite1");
+			fog2.setSpeedX(-1);
+			this.add(fog2);
+			this.player = new Player();
+			this.wall = new Wall(this);
+			this.add(this.player);
+			this.add(this.wall);
+		}; GameScene.prototype = Object.create(Scene.prototype);
+
+		// override
+		GameScene.prototype.getNext = function () {
+			return new GameScene();
+		};
+
+		// override
+		GameScene.prototype.getTransition = function () {
+			return new BaseTransition();
+		};
+
+		return GameScene;
+
+	})();
+
+	var Player = (function () {
+
+		var ANIMATION = new Animation([
+			new Frame(document.getElementById("birdSprite0"), 4),
+			new Frame(document.getElementById("birdSprite1"), 4),
+			new Frame(document.getElementById("birdSprite2"), 4)
+		]);
+
+		function Player() {
+			GameObject.call(this);
+			this.controller = Quick.getController();
+			this.setAnimation(ANIMATION);
+			this.setEssential();
+			this.setLayerIndex(1);
+			this.setSolid();
+			this.setSize(16);
+			this.setCenterX(Quick.getCanvasWidth() / 3);
+			this.setCenterY(Quick.getCanvasHeight() / 3);
+			this.setSpeedY(-7);
+		}; Player.prototype = Object.create(GameObject.prototype);
+
+		Player.prototype.flap = function () {
+			this.setImageId("birdSprite0");
+			this.setAnimation(ANIMATION);
+			this.setSpeedY(-7);
+			Quick.play("flapSound");
+		};
+
+		// override
+		Player.prototype.onCollision = function (gameObject) {
+			// checks for collisions with the columns
+			if (gameObject.hasTag("column")) {
+				Quick.play("fallSound");
+				this.expire();
+			}
+		};
+
+		// override
+		Player.prototype.update = function () {
+			// checks for falling off the bottom of the screen
+			if (this.getTop() > Quick.getCanvasHeight()) {
+				this.expire();
+			} else if (this.getTop() < 0) {
+				this.setTop(0);
+			}
+
+			// checks for space bar for flapping or just apply some gravity
+			if (this.controller.keyPush(CommandEnum.A)) {
+				this.flap();
+			} else if (this.getSpeedY() < 7) {
+				this.setSpeedY(this.getSpeedY() + 1);
+			}
+		};
+
+		return Player;
+
+	})();
+
+	var Wall = (function () {
+
+		function Wall(scene) {
+			GameObject.call(this);
+			this.scene = scene;
+			// the wall is only valid within our screen limits
+			this.setBoundary(new Rect(0, 0, Quick.getCanvasWidth(), Quick.getCanvasHeight()));
+			this.setColor("Gray");
+			this.setWidth(16);
+			this.setHeight(56);
+			this.setLeft(Quick.getCanvasWidth() - 1);
+			this.setTop(Quick.random(Quick.getCanvasHeight() - this.getHeight()));
+			this.setSpeedX(-2);
+
+			this.top = new Column();
+			this.top.setBottom(this.getTop() - 1);
+			this.top.setSpeedX(this.getSpeedX());
+			this.top.setWidth(this.getWidth());
+			this.top.setLeft(this.getLeft());
+			this.scene.add(this.top);
+
+			this.bottom = new Column();
+			this.bottom.setTop(this.getBottom() + 1);
+			this.bottom.setSpeedX(this.getSpeedX());
+			this.bottom.setWidth(this.getWidth());
+			this.bottom.setLeft(this.getLeft());
+			this.scene.add(this.bottom);
+		}; Wall.prototype = Object.create(GameObject.prototype);
+
+		// override
+		Wall.prototype.offBoundary = function () {
+			this.expire();
+			this.bottom.expire();
+			this.top.expire();
+			this.scene.add(new Wall(this.scene));
+		};
+
+		return Wall;
+
+	})();
+
+	main();
+
+})();
---
+++ demos/paddle/main.js
@@ -0,0 +1,154 @@
+(function () {
+
+	"use strict";
+
+	// imports
+	var CommandEnum = com.dgsprb.quick.CommandEnum;
+	var Quick = com.dgsprb.quick.Quick;
+    var GameObject = com.dgsprb.quick.GameObject;
+    var Rect = com.dgsprb.quick.Rect;
+	var Scene = com.dgsprb.quick.Scene;
+
+	// static
+	function main() {
+		Quick.setName("Paddle Demo");
+		Quick.init(document.getElementById("canvas"), FirstScene);
+	}
+
+	var Background = (function () {
+
+		function Background() {
+			GameObject.call(this);
+			this.setColor("Green");
+			this.setWidth(Quick.getCanvasWidth());
+			this.setHeight(Quick.getCanvasHeight());
+		}; Background.prototype = Object.create(GameObject.prototype);
+
+		return Background;
+	})();
+
+	var Ball = (function () {
+
+		function Ball() {
+			GameObject.call(this);
+			this.setImageId("ballSprite");
+			this.setBoundary(new Rect(0, 0, Quick.getCanvasWidth(), Quick.getCanvasHeight()));
+			this.setEssential();
+			this.setSolid();
+			this.setSpeedX(1 + Quick.random(3));
+			this.setSpeedY(1 + Quick.random(3));
+		}; Ball.prototype = Object.create(GameObject.prototype);
+
+		// override
+		Ball.prototype.onCollision = function (gameObject) {
+			var collision = this.getCollision(gameObject);
+			this.bounceFrom(collision);
+
+			if (gameObject.hasTag("paddle")) {
+				Quick.play("pingSound");
+			} else {
+				Quick.play("pongSound");
+			}
+		};
+
+		return Ball;
+
+	})();
+
+	var FirstScene = (function () {
+
+		function FirstScene() {
+			Scene.call(this);
+			this.add(new Background());
+			var horizontalPipe = new HorizontalPipe();
+			this.add(horizontalPipe);
+			var verticalPipe1, verticalPipe2;
+			verticalPipe1 = new VerticalPipe();
+			verticalPipe2 = new VerticalPipe();
+			verticalPipe2.setRight(Quick.getCanvasWidth());
+			this.add(verticalPipe1);
+			this.add(verticalPipe2);
+			var ball = new Ball();
+			ball.setTop(horizontalPipe.getBottom() + 1);
+			ball.setLeft(verticalPipe1.getRight() + 1);
+			this.add(ball);
+			this.add(new Paddle());
+			Quick.play("pongSound");
+		}; FirstScene.prototype = Object.create(Scene.prototype);
+
+		// override
+		FirstScene.prototype.getNext = function () {
+			console.log("#");
+			return new FirstScene();
+		};
+
+		return FirstScene;
+
+	})();
+
+	var Paddle = (function () {
+
+		function Paddle() {
+			GameObject.call(this);
+			this.addTag("paddle");
+			this.controller = Quick.getController();
+			this.setImageId("paddleSprite");
+			this.setSolid();
+			this.setCenterX(Quick.getCanvasWidth() / 2);
+			this.setBottom(Quick.getCanvasHeight() - this.getHeight());
+		}; Paddle.prototype = Object.create(GameObject.prototype);
+
+		// override
+		Paddle.prototype.update = function () {
+			if (this.controller.keyDown(CommandEnum.LEFT) && this.getLeft() > 0) {
+				this.moveX(-4);
+			}
+
+			if (this.controller.keyDown(CommandEnum.RIGHT) && this.getRight() < Quick.getCanvasWidth()) {
+				this.moveX(4);
+			}
+		};
+
+		return Paddle;
+
+	})();
+
+	var Pipe = (function () {
+
+		function Pipe() {
+			GameObject.call(this);
+			this.addTag("pipe");
+			this.setSolid();
+		}; Pipe.prototype = Object.create(GameObject.prototype);
+
+		return Pipe;
+
+	})();
+
+	var HorizontalPipe = (function () {
+
+		function HorizontalPipe() {
+			Pipe.call(this);
+			this.setImageId("horizontalPipeSprite");
+			this.setWidth(Quick.getCanvasWidth());
+		}; HorizontalPipe.prototype = Object.create(Pipe.prototype);
+
+		return HorizontalPipe;
+
+	})();
+
+	var VerticalPipe = (function () {
+
+		function VerticalPipe() {
+			Pipe.call(this);
+			this.setImageId("verticalPipeSprite");
+			this.setHeight(Quick.getCanvasHeight());
+		}; VerticalPipe.prototype = Object.create(Pipe.prototype);
+
+		return VerticalPipe;
+
+	})();
+
+	main();
+
+})();
