9d5b0d29ea5e618d6e4dd63b46819935098bd2f2
Diogo Schneider <dgsprb@gmail.com>
2015-04-24 20:40:48 -0300

    Add sandbox

---
+++ sandbox/main.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2015 Diogo Schneider
+ * 
+ * Made with Quick
+ * 
+ * https://github.com/dgsprb/quick
+ */
+
+(function () {
+
+	"use strict";
+
+	// imports
+	for (var i in com.dgsprb.quick) window[i] = com.dgsprb.quick[i];
+
+	// functions
+	function main() {
+		Quick.setName("Quick Sandbox");
+	}
+
+	main();
+
+})();
